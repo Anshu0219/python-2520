1212d2a83a94cf13a6102fbeb8e41b5d4a27e666
Anshu0219 <anshupolapally02@gmail.com>
2025-10-06 19:05:28 +0530

    identifers

---
+++ 01_basics/demo.py
@@ -0,0 +1,14 @@
+print("good moring")
+print(9)
+a=10
+b=21
+c=(a+b)
+print(c)
+print("anshu")
+name="ganesh"
+print(name)
+print(8)
+student_name="Ravi"
+student_id=23
+student_course="python"
+print(student_name,student_id,student_course)
