b75742212e5ed88dd1b0a0208e2f95709fae1c4b
Anshu0219 <anshupolapally02@gmail.com>
2025-10-06 19:36:41 +0530

    interpolations

---
+++ 02_variables/demo.py
@@ -0,0 +1,31 @@
+name = "Anshu"
+age = 21
+mark = 8.2
+print(name)  
+print(age)  
+print(mark)
+laptops_stock = 100
+laptops_scales = 24
+print(laptops_stock-laptops_scales)
+var = 10
+print(var)  
+print(type(var)) 
+print(id(var))
+print(var) 
+var = "Now I'm a string"
+print(var) 
+print(type(var))
+print(id(var))  
+var = 35.69
+print(var)  
+print(type(var))  
+print(id(var))
+num1 = 22
+num2 = 2.3
+num3 = 2.2
+print(type(num1))
+print(id(num1))
+print(type(num2))
+print(id(num2))
+print(type(num3))
+print(id(num3))
