84d3c40bb70d7a01212e6e678ddab8a643de9ede
Anshu0219 <anshupolapally02@gmail.com>
2025-10-08 11:50:54 +0530

    data type&converstion

---
+++ 04_data_types/demo.py
@@ -0,0 +1,28 @@
+data = [10,20,30,40,50]
+data = [101,"Ravi",True] 
+print(type(data))
+data = 10
+print(type(data))
+data = 10.4
+print(type(data))
+data = True
+print(type(data))
+data = 2+6j
+print(type(data))
+data = (7,5,4)
+print(type(data))
+data = "god"
+print(type(data))
+data = {1,2,3,4}
+print(type(data))
+data = {"name":"Og","age":22}
+print(type(data)) 
+a = 2
+print(type(a))
+b = 2.5
+print(type(b))
+c = a+b
+print(type(c))
+pi = 3.14 
+print(type(pi))
+print(pi) 
